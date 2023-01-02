b476f29072c405cc2254d8b5c142dbf8cc7ab2ac
estevaojneto <estevaojadanhi@gmail.com>
2023-01-01 23:41:04 -0300

    feat(mobile/frontend): small frontend fixes for mobile

--- web/app/themes/blogdynamic/template-part/homepage-posts.php
+++ web/app/themes/blogdynamic/template-part/homepage-posts.php
@@ -1,4 +1,4 @@
-<div class="row">
+<div class="row p-2">
     <div class="col-md-8 mx-auto">
         <div class="pb-2">
             <h1><a class="h1" href="<?= home_url() ?>/<?= $args['post_name'] ?>" data-post-link="<?= $args['post_name'] ?>"><?= $args['post_title'] ?></a></h1>
