d81c11b45743c0c9c3e2caef5c97423331deba3b
estevaojneto <estevaojadanhi@gmail.com>
2023-01-02 00:05:46 -0300

    feat(mobile/frontend): fix z-index

--- web/app/themes/blogdynamic/template-part/homepage-posts.php
+++ web/app/themes/blogdynamic/template-part/homepage-posts.php
@@ -1,5 +1,5 @@
 <div class="row p-2">
-    <div class="col-md-8 mx-auto">
+    <div class="col-md-8 mx-auto pb-3 container-postlist-post">
         <div class="pb-2">
             <h1><a class="h1" href="<?= home_url() ?>/<?= $args['post_name'] ?>" data-post-link="<?= $args['post_name'] ?>"><?= $args['post_title'] ?></a></h1>
             <p><em><?= wp_date( get_option('date_format'), strtotime($args['post_date'])); ?></em></p>
@@ -10,6 +10,5 @@
         </div>
         <br>
         <a class="h4" href="<?= home_url() ?>/<?= $args['post_name'] ?>" data-post-link="<?= $args['post_name'] ?>"><?= __('View Post', 'blogdynamic') ?></a>
-        <hr>
     </div>
 </div>
