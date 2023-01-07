09bdb134c288c859813b670ad9d56fc697cde9e6
estevaojneto <estevaojadanhi@gmail.com>
2023-01-07 12:44:23 -0300

    feat(index): have the index theme page load the React stuff

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php require 'frontend/public/index.html';
+<!doctype html>
+<html lang="en">
+  <head>
+    <title></title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <script defer src="/wp-content/themes/blog/frontend/build/static/js/main.min.js"></script>
+    <link rel="stylesheet" href="/wp-content/themes/blog/frontend/build/static/css/main.min.css">
+</head>
+<body>
+<div id="root"></div>
+</body>
+</html>
