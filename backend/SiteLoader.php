196aae5c1a019207c3a88b065f2e59e4fe3a9c2e
estevaojneto <estevaojadanhi@gmail.com>
2023-01-07 10:30:27 -0300

    feat(back): declare strict types in everything

--- backend/SiteLoader.php
+++ backend/SiteLoader.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Theme;
 
@@ -8,17 +8,17 @@ class SiteLoader
     public function __construct()
     {
         if (self::$currInstance === null) {
-            remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-            remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-            remove_action( 'wp_print_styles', 'print_emoji_styles' );
-            remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
-            remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-            remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
-            remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+            remove_action('wp_head', 'print_emoji_detection_script', 7);
+            remove_action('admin_print_scripts', 'print_emoji_detection_script');
+            remove_action('wp_print_styles', 'print_emoji_styles');
+            remove_action('admin_print_styles', 'print_emoji_styles'); 
+            remove_filter('the_content_feed', 'wp_staticize_emoji');
+            remove_filter('comment_text_rss', 'wp_staticize_emoji'); 
+            remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
             add_filter('xmlrpc_enabled', '__return_false');
-            remove_filter( 'the_title', 'capital_P_dangit', 11 );
-            remove_filter( 'the_content', 'capital_P_dangit', 11 );
-            remove_filter( 'comment_text', 'capital_P_dangit', 31 );
+            remove_filter('the_title', 'capital_P_dangit', 11);
+            remove_filter('the_content', 'capital_P_dangit', 11);
+            remove_filter('comment_text', 'capital_P_dangit', 31);
             add_filter('excerpt_length', function () { return 50; }, 999);
             self::$currInstance = $this;
         }
