ea480e5375819d87f79d792143257b6714754129
estevaojneto <estevaojadanhi@gmail.com>
2024-02-19 09:56:19 -0300

    fix(v2/styling): margins and alignments

--- single.php
+++ single.php
@@ -11,32 +11,34 @@ $background_style = $bg_img_src ?
 		<div class="container" style="min-height: 100vh">
             <div class="row">
                 <div class="col-lg-12">
-                    <div class="p-3">
+                    <div class="m-3">
                         <?php if($bg_img_src){?>
                         <div <?= $background_style ?> class="p-3 single-hero-background"></div>
                         <?php }?>
                     </div>
 				</div>
                 <div class="col-lg-4">
-                    <div>
+                    <div class="m-3">
                         <div class="row">
-                            <div class="p-3 col-lg-3 single-menu-side-fixed">
+                            <div class="col-lg-3 single-menu-side-fixed">
                                 <h1><?= $post->post_title ?></h1>
                                 <p class="h5"><em><?= get_the_date(); ?></em></p>
                                 <div class="row">
-                                    <div class="col-sm-6 text-left">
+                                    <div class="col-6 text-left">
                                         <span class="h5">
                                             <a href="#"><< Prev</a>
                                         </span>
                                     </div>
-                                    <div class="col-sm-6 text-right" style="text-align:right!important">
+                                    <div class="col-6 text-right" style="text-align:right!important">
                                         <span class="h5">
                                             <a href="#">>> Next</a>
                                         </span>
                                     </div>
-                                    <div class="col-md-12 mb-4 mt-3 post-card-container">
-                                        <div class="outline single-site-card site-card p-0 m-0">
-                                            <div class="d-flex align-items-center h-50 w-50 justify-content-center" style="border: 2px solid">
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12 post-card-container">
+                                        <div class="outline single-site-card site-card">
+                                            <div class="d-flex align-items-center h-50 w-75 justify-content-center" style="border: 2px solid">
                                                 <h1 class="h1 blogtitle"><a class="blog-link-card" href="<?= home_url() ?>"><?= get_bloginfo("name") ?></a></h1>
                                             </div>
                                         </div>
@@ -47,7 +49,7 @@ $background_style = $bg_img_src ?
                     </div>
                 </div>
                 <div class="col-lg-8">
-                    <div class="p-3">
+                    <div class="m-3">
                         <div class="outline">
                             <div class="p-3 single-post-contents">
                                 <?= apply_filters('the_content', $post->post_content) ?>
