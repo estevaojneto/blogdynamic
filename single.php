839ad22f2e664c1d76c074ebf6de41376d207597
Estevão Neto <37549773+estevaojneto@users.noreply.github.com>
2024-12-13 08:33:33 -0300

    Update single.php

--- single.php
+++ single.php
@@ -7,77 +7,40 @@ $background_style = $bg_img_src ?
     "style='background:url($bg_img_src)';" : '';
 ?>
 <!doctype html>
-<html lang="en">
-	<?= get_header() ?>
-	<body>
-		<div class="container" style="min-height: 100vh">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="m-3">
-                        <?php if($bg_img_src){?>
-                        <div <?= $background_style ?> class="p-3 single-hero-background"></div>
-                        <?php }?>
-                    </div>
-				</div>
-                <div class="col-lg-4">
-                    <div class="m-3">
-                        <div class="single-menu-side-fixed">
-                                <h1><?= $post->post_title ?></h1>
-                                <p class="h5"><em><?= get_the_date(); ?></em></p>
-                                <div class="row">
-                                    <div class="col-6 text-left">
-					<?php if(prev_post_link) {?>
-	                                        <p class="m-0 p-0"><small>Previous post</small></p>
-	                                        <span class="h5">
-	                                            <?= $prev_post_link ?>
-	                                        </span>
-					<?php } ?>
-                                    </div>
-                                    <div class="col-6 text-right" style="text-align:right!important">
-					<?php if($next_post_link) {?>
-	                                        <p class="m-0 p-0"><small>Next post</small></p>
-	                                        <span class="h5">
-	                                            <?= $next_post_link ?>
-	                                        </span>
-					<?php } >
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12 post-card-container">
-                                        <div class="outline single-site-card site-card">
-                                            <div class="d-flex align-items-center h-50 w-75 justify-content-center" style="border: 2px solid">
-                                                <h1 class="h1 blogtitle"><a class="blog-link-card" href="<?= home_url() ?>"><?= get_bloginfo("name") ?></a></h1>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                    </div>
-                </div>
-                <div class="col-lg-8">
-                    <div class="m-3">
-                        <div class="outline">
-                            <div class="p-3 single-post-contents">
-                                <?= apply_filters('the_content', $post->post_content) ?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-                <?php /* get_template_part('comments');
-                $comments = get_comments(array(
-                    'post_id' => $post->ID,
-                    'status' => 'approve' //Change this to the type of comments to be displayed
-                ));
-        
-                //Display the list of comments
-                wp_list_comments(array(
-                    'per_page' => 10, //Allow comment pagination
-                    'reverse_top_level' => false //Show the oldest comments at the top of the list
-                ), $comments);*/
-                ?>
+<html lang="en"> <?= get_header() ?><body>
+    <div class="container" style="min-height: 100vh">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="m-3"> <?php if($bg_img_src){?><div <?= $background_style ?> class="p-3 single-hero-background"></div> <?php }?></div>
         </div>
-		<?= get_footer() ?>
-	</body>
-
+        <div class="col-lg-4">
+          <div class="single-menu-side-fixed m-3">
+            <h1> <?= $post->post_title ?></h1>
+            <p class="h5"><em> <?= get_the_date(); ?></em></p>
+            <div class="row">
+              <div class="col-6 text-left"> <?php if(!empty($prev_post_link)) {?><p class="m-0 p-0"><small>Previous post</small></p><span class="h5"> <?= $prev_post_link ?></span> <?php } ?></div>
+              <div class="col-6 text-right" style="text-align:right!important"> <?php if(!empty($next_post_link)) {?><p class="m-0 p-0"><small>Next post</small></p><span class="h5"> <?= $next_post_link ?></span> <?php } ?></div>
+            </div>
+            <div class="row">
+              <div class="col-md-12 post-card-container">
+                <div class="outline single-site-card site-card">
+                  <div class="d-flex align-items-center h-50 w-75 justify-content-center" style="border: 2px solid">
+                    <h1 class="h1 blogtitle"><a class="blog-link-card" href="
+<?= home_url() ?>"> <?= get_bloginfo("name") ?></a></h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-8">
+          <div class="m-3">
+            <div class="outline">
+              <div class="p-3 single-post-contents"> <?= apply_filters('the_content', $post->post_content) ?></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> <?= get_footer() ?>
+  </body>
 </html>
